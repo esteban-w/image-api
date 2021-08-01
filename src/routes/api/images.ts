6ea30cb0c39bf1602a696fdb45446511db794d17
ESTEBAN <esteban.v.web@gmail.com>
2021-08-01 16:31:23 -0500

    implement image route with filename query parameter

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,19 +1,10 @@
-import express, {Router} from 'express';
+import {Response, Router} from 'express';
 import imageProcessing from '../../imageProcessing';
 
 const images = Router();
-
-images.get('/:imageName(\\w+).:imageExt(gif|png|jpe?g)', async (req, res) => {
-    const imageName = req.params.imageName;
-    const imageExt = req.params.imageExt;
-
+const fileHandler = async (res: Response, imageName: string, imageExt: string, imageWidth: string, imageHeight: string) => {
     try {
-        const imageToSend = await imageProcessing(
-            imageName,
-            imageExt,
-            typeof req.query.width === 'string' ? req.query.width : '',
-            typeof req.query.height === 'string' ? req.query.height : ''
-        );
+        const imageToSend = await imageProcessing(imageName, imageExt, imageWidth, imageHeight);
 
         res.sendFile(imageToSend, {maxAge: '2 days'}, err => {
             if (err) {
@@ -23,6 +14,34 @@ images.get('/:imageName(\\w+).:imageExt(gif|png|jpe?g)', async (req, res) => {
     } catch {
         res.status(404).send(`Unable to find file: ${imageName}.${imageExt}`)
     }
+}
+
+images.get('/',  (req, res) => {
+    const filename = req.query.filename;
+    const filenameMatches = typeof filename === 'string'
+        ? filename.match(/(\w+)\.(gif|png|jpe?g)$/) : null;
+
+    if (filenameMatches) {
+        fileHandler(
+            res,
+            filenameMatches[1],
+            filenameMatches[2],
+            typeof req.query.width === 'string' ? req.query.width : '',
+            typeof req.query.height === 'string' ? req.query.height : ''
+        );
+    } else {
+        res.send('No file was specified correctly')
+    }
+})
+
+images.get('/:imageName(\\w+).:imageExt(gif|png|jpe?g)', async (req, res) => {
+    fileHandler(
+        res,
+        req.params.imageName,
+        req.params.imageExt,
+        typeof req.query.width === 'string' ? req.query.width : '',
+        typeof req.query.height === 'string' ? req.query.height : ''
+    );
 })
 
 export default images;
