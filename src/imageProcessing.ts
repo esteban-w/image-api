72a3951578fd731556754a12005a13a452bcb0aa
ESTEBAN <esteban.v.web@gmail.com>
2021-08-01 15:14:29 -0500

    implement image processing as an independent module

---
+++ src/imageProcessing.ts
@@ -0,0 +1,34 @@
+import sharp from 'sharp';
+import path from 'path';
+import {constants, promises as fsPromises} from 'fs';
+
+const imageProcessing = async (imageName:string, imageExt:string, imageWidth:string, imageHeight:string): Promise<string> => {
+    const assetsPath = path.join(__dirname, '../assets/full/');
+    const assetsThumbsPath = path.join(__dirname, '../assets/thumbs/');
+    const imageFullPath = `${assetsPath}${imageName}.${imageExt}`;
+
+    // check if image requested does in fact exist
+    await fsPromises.access(imageFullPath, constants.F_OK);
+
+    // make sure both width and height are truthy values and they're number values
+    if (imageWidth && imageHeight && !isNaN(parseInt(imageWidth)) && !isNaN(parseInt(imageHeight))) {
+        const resizedImagePath = `${assetsThumbsPath}${imageName}${imageWidth}x${imageHeight}.${imageExt}`;
+
+        // check if resized image already exists, otherwise create it
+        try {
+            await fsPromises.access(resizedImagePath, constants.F_OK);
+        } catch {
+            await sharp(imageFullPath)
+                .resize(parseInt(imageWidth), parseInt(imageHeight))
+                .toFile(resizedImagePath);
+        }
+        return new Promise(resolve => {
+            resolve(resizedImagePath)
+        });
+    }
+    return new Promise(resolve => {
+        resolve(imageFullPath)
+    });
+}
+
+export default imageProcessing;
--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,22 +1,28 @@
 import express, {Router} from 'express';
-import {constants, promises as fsPromises} from 'fs';
-import path from "path";
+import imageProcessing from '../../imageProcessing';
 
 const images = Router();
-const absoluteAssetsPath = path.join(__dirname, '../../../assets/full/');
 
-images.get('/:imageFile(\\w+\.(?:gif|png|jpe?g)$)', (req, res) => {
+images.get('/:imageName(\\w+).:imageExt(gif|png|jpe?g)', (req, res) => {
     (async () => {
-        try {
-            await fsPromises.access(`${absoluteAssetsPath}${req.params.imageFile}`, constants.F_OK)
+        const imageName = req.params.imageName;
+        const imageExt = req.params.imageExt;
 
-            res.sendFile(`${absoluteAssetsPath}${req.params.imageFile}`, {maxAge: '2 days'}, err => {
+        try {
+            const imageToSend = await imageProcessing(
+                imageName,
+                imageExt,
+                typeof req.query.width === 'string' ? req.query.width : '',
+                typeof req.query.height === 'string' ? req.query.height : ''
+            );
+
+            res.sendFile(imageToSend, {maxAge: '2 days'}, err => {
                 if (err) {
                     res.status(500).send('An error has occurred while sending the requested asset')
                 }
             })
         } catch {
-            res.status(404).send(`File: ${req.params.imageFile} , Not Found`)
+            res.status(404).send(`Unable to find file: ${imageName}.${imageExt}`)
         }
     })();
 })
