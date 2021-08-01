9088eaee5d1085541c6d8f13695fa9e585028578
ESTEBAN <esteban.v.web@gmail.com>
2021-08-01 15:20:17 -0500

    implement images route handler as async function

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -3,28 +3,26 @@ import imageProcessing from '../../imageProcessing';
 
 const images = Router();
 
-images.get('/:imageName(\\w+).:imageExt(gif|png|jpe?g)', (req, res) => {
-    (async () => {
-        const imageName = req.params.imageName;
-        const imageExt = req.params.imageExt;
+images.get('/:imageName(\\w+).:imageExt(gif|png|jpe?g)', async (req, res) => {
+    const imageName = req.params.imageName;
+    const imageExt = req.params.imageExt;
 
-        try {
-            const imageToSend = await imageProcessing(
-                imageName,
-                imageExt,
-                typeof req.query.width === 'string' ? req.query.width : '',
-                typeof req.query.height === 'string' ? req.query.height : ''
-            );
+    try {
+        const imageToSend = await imageProcessing(
+            imageName,
+            imageExt,
+            typeof req.query.width === 'string' ? req.query.width : '',
+            typeof req.query.height === 'string' ? req.query.height : ''
+        );
 
-            res.sendFile(imageToSend, {maxAge: '2 days'}, err => {
-                if (err) {
-                    res.status(500).send('An error has occurred while sending the requested asset')
-                }
-            })
-        } catch {
-            res.status(404).send(`Unable to find file: ${imageName}.${imageExt}`)
-        }
-    })();
+        res.sendFile(imageToSend, {maxAge: '2 days'}, err => {
+            if (err) {
+                res.status(500).send('An error has occurred while sending the requested asset')
+            }
+        })
+    } catch {
+        res.status(404).send(`Unable to find file: ${imageName}.${imageExt}`)
+    }
 })
 
 export default images;
